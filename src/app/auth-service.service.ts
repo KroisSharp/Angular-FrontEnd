572566cf11420dae0b49bbabcd18d8a854824e73
Krois <kroismikdev@gmail.com>
2019-01-13 14:44:40 +0100

    ved 200 navigate til mainpage

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -4,20 +4,22 @@ import * as firebase from 'firebase/app';
 import { promise } from 'protractor';
 import { resolve, reject } from 'q';
 import { SignInComponent } from './sign-in/sign-in.component';
+import {Router} from '@angular/router'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth,
+    private Router : Router) { }
 
   DoLogin(Username, Password) {
     return new Promise<any>((resolve, reject) => {
       firebase.auth().signInWithEmailAndPassword(Username, Password)
         .then(res => {
-          console.log(res)
           resolve(res);
+          this.Router.navigate(['/mainmenu'])
         }, err => {
           reject(err)
           console.log(err)
