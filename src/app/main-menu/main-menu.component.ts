7dcc7eddcaf20f4be648465b3f4230fc857dca76
Krois <kroismikdev@gmail.com>
2019-01-16 11:02:09 +0100

    mindre ærding til liste - lavet rigtige navne samt ændret css

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -3,21 +3,20 @@ import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  Price: number;
+  Department: string;
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {name: 'Yoghurt', Price: 12.95, Department: 'Mejeri'},
+  {name: 'Mælk', Price: 7.95, Department: 'Mejeri'},
+  {name: 'Fløde', Price: 6.95, Department: 'Mejeri'},
+  {name: 'æg', Price: 19.95, Department: 'Mejeri'},
+  {name: 'Kylling', Price: 45.0, Department: 'Kød'},
+  {name: 'Steak', Price: 250.0, Department: 'Kød'},
+  {name: 'Chips', Price: 14.95, Department: 'Slik'},
+  {name: 'vingummi', Price: 19.95, Department: 'Slik'},
+  {name: 'Sæbe', Price: 10.0, Department: 'Husholdning'},
+  { name: 'skraldepose', Price: 8.95, Department: 'Husholdning'},
 ];
 
 @Component({
@@ -34,7 +33,7 @@ export class MainMenuComponent implements OnInit {
   }
 
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['name', 'Price', 'Department'];
   dataSource = ELEMENT_DATA;
 
 }
